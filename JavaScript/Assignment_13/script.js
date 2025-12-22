5ba4a3e52b9c97ad303016b111ffd2d670d1a9d3
Moein-Moatali-2006 <MoeinMoatali@gmail.com>
2025-12-23 00:31:04 +0330

    A-13

--- JavaScript/Assignment_13/script.js
+++ JavaScript/Assignment_13/script.js
@@ -1,4 +1,14 @@
 var cell = document.getElementsByClassName("cell");
+var show_winner = document.getElementById("show_winner_text");
+var new_game = document.getElementById("new_game");
+
+var score_X_f = document.getElementById("score_X");
+var score_O_f = document.getElementById("score_O");
+var score_draw_f = document.getElementById("score_draw");
+
+var score_X_b = 0;
+var score_O_b = 0;
+var score_Draw_b = 0;
 
 var main_board_f = [
     [cell[0], cell[1], cell[2]],
@@ -14,7 +24,6 @@ var main_board_b = [
 
 var ply = "player X"
 
-
 function game(row, col){
     if (main_board_b[row][col] == null){
 
@@ -24,6 +33,7 @@ function game(row, col){
             main_board_f[row][col].innerHTML = "X";
             main_board_f[row][col].style.color = "red";
             ply = "player O";
+            check()
         }
         else if (ply ==  "player O"){
             main_board_b[row][col] = "O";
@@ -31,9 +41,138 @@ function game(row, col){
             main_board_f[row][col].innerHTML = "O";
             main_board_f[row][col].style.color = "blue";
             ply = "player X";
+            check()
         }
     }
     else{
         alert("This cell isn't null !");
     }
 }
+
+function check(){
+    if (main_board_b[0][0] == "X" && main_board_b[0][1] == "X" && main_board_b[0][2] == "X"){
+        score_X_b ++;
+        score_X_f.innerHTML = score_X_b;
+        show_winner.innerHTML = "X Won!!!"
+        return false;
+    }
+    else if (main_board_b[1][0] == "X" && main_board_b[1][1] == "X" && main_board_b[1][2] == "X"){
+        score_X_b ++;
+        score_X_f.innerHTML = score_X_b;
+        show_winner.innerHTML = "X Won!!!"
+        return false;
+    }
+    else if (main_board_b[2][0] == "X" && main_board_b[2][1] == "X" && main_board_b[2][2] == "X"){
+        score_X_b ++;
+        score_X_f.innerHTML = score_X_b;
+        show_winner.innerHTML = "X Won!!!"
+        return false;
+    }
+    else if (main_board_b[0][0] == "X" && main_board_b[1][0] == "X" && main_board_b[2][0] == "X"){
+        score_X_b ++;
+        score_X_f.innerHTML = score_X_b;
+        show_winner.innerHTML = "X Won!!!"
+        return false;
+    }
+    else if (main_board_b[0][1] == "X" && main_board_b[1][1] == "X" && main_board_b[2][1] == "X"){
+        score_X_b ++;
+        score_X_f.innerHTML = score_X_b;
+        show_winner.innerHTML = "X Won!!!"
+        return false;
+    }
+    else if (main_board_b[0][2] == "X" && main_board_b[1][2] == "X" && main_board_b[2][2] == "X"){
+        score_X_b ++;
+        score_X_f.innerHTML = score_X_b;
+        show_winner.innerHTML = "X Won!!!"
+        return false;
+    }
+    else if (main_board_b[0][2] == "X" && main_board_b[1][1] == "X" && main_board_b[2][0] == "X"){
+        score_X_b ++;
+        score_X_f.innerHTML = score_X_b;
+        show_winner.innerHTML = "X Won!!!"
+        return false;
+    }
+    else if (main_board_b[0][0] == "X" && main_board_b[1][1] == "X" && main_board_b[2][2] == "X"){
+        score_X_b ++;
+        score_X_f.innerHTML = score_X_b;
+        show_winner.innerHTML = "X Won!!!"
+        return false;
+    }
+
+
+    if (main_board_b[0][0] == "O" && main_board_b[0][1] == "O" && main_board_b[0][2] == "O"){
+        score_O_b ++;
+        score_O_f.innerHTML = score_O_b;
+        show_winner.innerHTML = "O Won!!!"
+        return false;
+    }
+    else if (main_board_b[1][0] == "O" && main_board_b[1][1] == "O" && main_board_b[1][2] == "O"){
+        score_O_b ++;
+        score_O_f.innerHTML = score_O_b;
+        show_winner.innerHTML = "O Won!!!"
+        return false;
+    }
+    else if (main_board_b[2][0] == "O" && main_board_b[2][1] == "O" && main_board_b[2][2] == "O"){
+        score_O_b ++;
+        score_O_f.innerHTML = score_O_b;
+        show_winner.innerHTML = "O Won!!!"
+        return false;
+    }
+    else if (main_board_b[0][0] == "O" && main_board_b[1][0] == "O" && main_board_b[2][0] == "O"){
+        score_O_b ++;
+        score_O_f.innerHTML = score_O_b;
+        show_winner.innerHTML = "O Won!!!"
+        return false;
+    }
+    else if (main_board_b[0][1] == "O" && main_board_b[1][1] == "O" && main_board_b[2][1] == "O"){
+        score_O_b ++;
+        score_O_f.innerHTML = score_O_b;
+        show_winner.innerHTML = "O Won!!!"
+        return false;
+    }
+    else if (main_board_b[0][2] == "O" && main_board_b[1][2] == "O" && main_board_b[2][2] == "O"){
+        score_O_b ++;
+        score_O_f.innerHTML = score_O_b;
+        show_winner.innerHTML = "O Won!!!"
+        return false;
+    }
+    else if (main_board_b[0][2] == "O" && main_board_b[1][1] == "O" && main_board_b[2][0] == "O"){
+        score_O_b ++;
+        score_O_f.innerHTML = score_O_b;
+        show_winner.innerHTML = "O Won!!!"
+        return false;
+    }
+    else if (main_board_b[0][0] == "O" && main_board_b[1][1] == "O" && main_board_b[2][2] == "O"){
+        score_O_b ++;
+        score_O_f.innerHTML = score_O_b;
+        show_winner.innerHTML = "O Won!!!"
+        return false;
+    }
+
+    var draw = 0;
+    for (let i = 0; i <= 2; i++) {
+        for (let j = 0; j <= 2; j++) {
+            if (main_board_b[i][j] != null) {
+                draw ++;
+            }
+        }
+    }
+    if(draw == 9){
+        score_Draw_b ++;
+        score_draw_f.innerHTML = score_Draw_b;
+        show_winner.innerHTML = "Draw!!"
+        return false;
+    }
+}
+
+new_game.onclick = function(){
+    for (let i = 0; i <= 2; i++) {
+        for (let j = 0; j <= 2; j++) {
+            main_board_b[i][j] = null;
+            main_board_f[i][j].style.backgroundColor = "rgba(60, 60, 60, 0.5)";
+            main_board_f[i][j].innerHTML = "";
+        }
+    }
+}
+
+
